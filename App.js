f83776616c83df6d0c69a4a610bc7da907c12f96
Ali Milani <haythamtemplar@hotmail.com>
2022-03-31 20:49:24 +0430

    Dispatch function created:   App.js

--- App.js
+++ App.js
@@ -16,7 +16,79 @@ const initialState = {
   operator_type: "",
   number: 0,
 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.RESET_ALL:
+      return {
+        first_value: "",
+        second_value: "",
+        operator_type: "",
+        is_operator_got: false,
+        number: 0,
+        result: 0,
+      };
+    case ACTIONS.GET_VALUE:
+      if (state.is_operator_got === false) {
+        return {
+          ...state,
+          first_value: state.first_value.concat(action.payload.value),
+        };
+      }
+      return {
+        ...state,
+        second_value: state.second_value.concat(action.payload.value),
+      };
+    case ACTIONS.GET_OPERATOR:
+      return {
+        ...state,
+        operator_type: action.payload.value,
+        is_operator_got: true,
+      };
+    case ACTIONS.GET_RESULT:
+      switch (state.operator_type) {
+        case "+":
+          return {
+            ...state,
+            number: Number(state.first_value) + Number(state.second_value),
+            operator_type: "=",
+          };
+        case "-":
+          return {
+            ...state,
+            number: Number(state.first_value) - Number(state.second_value),
+            operator_type: "=",
+          };
+        case "*":
+          return {
+            ...state,
+            number: Number(state.first_value) * Number(state.second_value),
+            operator_type: "=",
+          };
+        case "/":
+          return {
+            ...state,
+            number: Number(state.first_value) / Number(state.second_value),
+            operator_type: "=",
+          };
 
+        default:
+          return { state };
+      }
+    case ACTIONS.GET_CONTINUE:
+      return {
+        ...state,
+        first_value: state.number,
+        second_value: "",
+      };
+    case ACTIONS.SET_FIRST_BACK:
+      return {
+        ...state,
+        first_value: "",
+      };
+    default:
+      return state;
+  }
+};
 function App() {
   return <div></div>;
 }
