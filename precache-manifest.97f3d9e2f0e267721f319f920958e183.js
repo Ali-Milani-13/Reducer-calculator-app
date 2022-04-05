207e794d0258f0da97cc7148f8da7b07fcd9c564
Ali Milani <haythamtemplar@hotmail.com>
2022-04-05 07:58:14 +0430

    Updates

---
+++ precache-manifest.97f3d9e2f0e267721f319f920958e183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8318d90952c7fb38ddebd72ce5f064cf",
+    "url": "/Reducer-calculator-app/index.html"
+  },
+  {
+    "revision": "e4920adfcfb208e7ca43",
+    "url": "/Reducer-calculator-app/static/css/main.5d08cac2.chunk.css"
+  },
+  {
+    "revision": "c8d73522aa435610f076",
+    "url": "/Reducer-calculator-app/static/js/2.18e95ca5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reducer-calculator-app/static/js/2.18e95ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4920adfcfb208e7ca43",
+    "url": "/Reducer-calculator-app/static/js/main.2a99ca62.chunk.js"
+  },
+  {
+    "revision": "24ba175b13fbcab181b2",
+    "url": "/Reducer-calculator-app/static/js/runtime-main.7230af1c.js"
+  }
+]);
